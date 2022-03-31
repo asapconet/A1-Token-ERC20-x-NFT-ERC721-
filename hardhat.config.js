84f44cdfebfce25ae69d18559ce1f6482d8f6c22
asapconet <aaronrickymeek@gmail.com>
2022-03-31 12:15:38 +0100

    Bug: gitignore protection

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,40 +5,11 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 const { API_URL, PRIVATE_KEY, API_KEY } = process.env;
 module.exports = {
-    defaultNetwork: "rinkeby",
-  networks: {
-    hardhat: {
-    },
-    rinkeby: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    }
-  },
-  solidity: {
-    version: "0.8.1",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 40000
-  },
-
-  solidity: "0.8.1",
   defaultNetwork: "rinkeby",
   networks: {
     hardhat: {},
     rinkeby: {
-      url: [API_URL],
+      url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
     etherscan: {
@@ -46,23 +17,56 @@ module.exports = {
       // Obtain one at https://etherscan.io/
       apiKey: API_KEY,
     },
-    solidity: {
-      version: "0.8.1",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200,
-        },
+  },
+  solidity: {
+    version: "0.8.1",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
-    paths: {
-      sources: "./contracts",
-      tests: "./test",
-      cache: "./cache",
-      artifacts: "./artifacts",
-    },
-    mocha: {
-      timeout: 40000,
-    },
   },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+
+  // solidity: "0.8.1",
+  // defaultNetwork: "rinkeby",
+  // networks: {
+  //   hardhat: {},
+  //   rinkeby: {
+  //     url: [API_URL],
+  //     accounts: [`0x${PRIVATE_KEY}`],
+  //   },
+  //   etherscan: {
+  //     // Your API key for Etherscan
+  //     // Obtain one at https://etherscan.io/
+  //     apiKey: API_KEY,
+  //   },
+  //   solidity: {
+  //     version: "0.8.1",
+  //     settings: {
+  //       optimizer: {
+  //         enabled: true,
+  //         runs: 200,
+  //       },
+  //     },
+  //   },
+  //   paths: {
+  //     sources: "./contracts",
+  //     tests: "./test",
+  //     cache: "./cache",
+  //     artifacts: "./artifacts",
+  //   },
+  //   mocha: {
+  //     timeout: 40000,
+  //   },
+  // },
 };
